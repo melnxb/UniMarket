66b307e2b9422924d6dd108f1c5a4d5138e8e021
Pedro Bastos Leite <pedrobastos747@gmail.com>
2023-10-24 12:22:29 -0300

    Imagens funcionando

--- Code/PastaJS/script.js
+++ Code/PastaJS/script.js
@@ -13,7 +13,7 @@ window.onload = async function(){
                     <div class="col-12">
                         <div class="card d-inline-block my-2 mx-2">
                             <div class="card-body text-center">
-                                <img src="../Data/ImgProd/${[i]}.png" class="card-img-top" alt="Imagem Produto">
+                                <img src="../Data/ImgProd/${dados[i].id_produto}.png" class="card-img-top" alt="Imagem Produto">
                                 <h5 class="card-title">${dados[i].nome}</h5>
                                 <p class="card-text">R$ ${dados[i].preco}</p>
                                 <a href="#" class="btn btn-primary col-12">Adicionar ao carrinho!</a>
