3e88339d8292d2af70719837c56875a8e6b5a9b7
Melyça <80221337+melnxb@users.noreply.github.com>
2023-10-29 19:05:02 -0300

    Update excluirProduto.php

--- Code/PastaPHP/excluirProduto.php
+++ Code/PastaPHP/excluirProduto.php
@@ -2,7 +2,7 @@
     
     $id_produto = $_POST['id_produto'];
     if($id_produto){
-        $con = mysqli_connect("localhost:3306", "root", "Melyca1243!", "unimarket");
+        $con = mysqli_connect("localhost:3306", "root", "", "unimarket");
         $query= "DELETE FROM produtos WHERE id_produto=$id_produto";
         $resultado = mysqli_query($con, $query);
         
